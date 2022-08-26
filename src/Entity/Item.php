bd06afa8ba5715c9377f298f5d9792914426e9ba
Damien Mauchamp <d.mauchamp@login-info.com>
2022-08-26 20:05:19 +0200

    Displaying items

--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Repository\IssueRepository;
 use App\Repository\ItemRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -40,6 +41,13 @@ class Item {
 
 	#[ORM\OneToMany(mappedBy: 'item', targetEntity: ItemIssue::class)]
 	private $issues;
+
+	private ?Issue $last_read_issue = null;
+	private ?Issue $next_to_read_issue = null;
+	private array $issues_read = [];
+	private int $number_of_issues_read = 0;
+	private ?int $number_issues = null;
+	private array $next_to_read_issues = [];
 
 	#[ORM\Column(type: 'datetime')]
 	private $date_added;
@@ -197,8 +205,286 @@ class Item {
 		return "$name.{$ext}";
 	}
 
+	public function getCollectionName(): string {
+		return $this->getItemCollection()->getName();
+	}
+
+	public function getCollectionTypeName(): string {
+		return $this->getItemCollection()->getType()->getName();
+	}
+
 	public function getFullName(): string {
 		$number = $this->getNumber() ? ", tome {$this->getNumber()} " : '';
 		return trim($this->getItemCollection()->getName()."{$number} - ".$this->getTitle(), ' -');
+	}
+
+	/**
+	 * @return Issue|null
+	 */
+	public function getLastReadIssue(): ?Issue {
+		if($this->last_read_issue) {
+			return $this->last_read_issue;
+		}
+//		return $this->issues_read[0] ?? null;
+		if(!$this->issues) {
+			return null;
+		}
+
+//		$readIssues = $this->issues->filter(function ($itemIssue) {
+//			/** @var ItemIssue $itemIssue */
+//			return $itemIssue->getIssue()->isRead();
+//		});
+//		if(!$readIssues) {
+		if(!$this->issues_read) {
+			return null;
+		}
+
+//		if(count($this->issues_read) === 1) {
+//			$this->last_read_issue = $this->issues_read[0]->getIssue();
+//			return $this->last_read_issue;
+//		}
+//
+//		foreach($this->issues_read as $itemIssue) {
+//			/** @var ItemIssue $itemIssue */
+//			if(!$this->last_read_issue ||
+//				$itemIssue->getIssue()->getDateRead() > $this->last_read_issue->getDateRead()) {
+//				$this->last_read_issue = $itemIssue->getIssue();
+//			}
+//		}
+
+		return $this->last_read_issue;
+
+
+//		dd($readIssues);
+
+
+	}
+
+	/**
+	 * @param Issue|null $last_read_issue
+	 */
+	public function setLastReadIssue(?Issue $last_read_issue): void {
+		$this->last_read_issue = $last_read_issue;
+	}
+
+	/**
+	 * @return Issue|null
+	 */
+	public function getNextToreadissue(): ?Issue {
+		return $this->next_to_read_issue;
+	}
+
+	/**
+	 * @param Issue|null $next_to_read_issue
+	 */
+	public function setNextToReadIssue(?Issue $next_to_read_issue): void {
+		$this->next_to_read_issue = $next_to_read_issue;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getIssuesRead(): array {
+		return $this->issues_read;
+	}
+
+	/**
+	 * @param array $issues_read
+	 */
+	public function setIssuesRead(array $issues_read): void {
+		$this->issues_read = $issues_read;
+		$this->number_of_issues_read = count($this->issues_read);
+	}
+
+	public function setIssuesProgress(): void {
+
+		$this->number_issues = count($this->getIssues());
+
+//		$itemIssues = $this->getIssues()->getValues();
+
+		$issuesRead = $this->issues->filter(function ($itemIssue) {
+			/** @var ItemIssue $itemIssue */
+			return $itemIssue->getIssue()->isRead();
+		});
+		$this->setIssuesRead($issuesRead->toArray());
+
+		$issueLastRead = null;
+		if(count($this->issues_read) === 1) {
+			$issueLastRead = array_values($this->issues_read)[0]->getIssue();
+		}
+		else {
+			foreach($this->issues_read as $itemIssue) {
+				/** @var ItemIssue $itemIssue */
+				if(!$issueLastRead ||
+					$itemIssue->getIssue()->getDateRead() > $issueLastRead->getDateRead()) {
+					$issueLastRead = $itemIssue->getIssue();
+				}
+			}
+		}
+		$this->setLastreadissue($issueLastRead);
+
+		//
+		$issueNextToRead = null;
+		if($issueLastRead) {
+//			$next = false;
+			foreach($this->issues as $itemIssue) {
+				/** @var ItemIssue $itemIssue */
+				if(!$itemIssue->getIssue()->isRead()) {
+					$issueNextToRead = $itemIssue->getIssue();
+					break;
+				}
+//				if($next) {
+//					$issueNextToRead = $itemIssue->getIssue();
+//					break;
+//				}
+//				if($itemIssue->getIssue()->getId() === $issueLastRead->getId()) {
+//					$next = true;
+//				}
+			}
+		}
+		$this->setNextToReadIssue($issueNextToRead);
+
+		$this->setNextToReadIssues();
+	}
+
+
+	public function getNumberIssues(): ?int {
+		return $this->number_issues;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getNumberOfIssuesRead(): int {
+		return $this->number_of_issues_read;
+	}
+
+	public function getRemainingIssues(int $sub = 0): int {
+		return $this->number_issues - $this->number_of_issues_read - $sub;
+	}
+
+	public function getRemainingIssuesToString(int $sub = 0): string {
+		if(!$this->getNumberOfIssuesRead()) {
+			return "{$this->getNumberIssues()} issues";
+		}
+		return "{$this->getNumberOfIssuesRead()}/{$this->getNumberIssues()} issues";
+//		return $count > 0 ? ($sub ? "+{$count} more" : "{$count} issues") : '';
+	}
+
+	public function isStarted(): bool {
+		return $this->getNumberOfIssuesRead() > 0;
+	}
+
+	public function isDone(): bool {
+		return $this->getRemainingIssues() === 0;
+	}
+
+	public function getProgress(): float {
+		if($this->isDone()) {
+			return 100;
+		}
+		return $this->number_of_issues_read / $this->number_issues * 100;
+	}
+
+	public function getIssuesArray(): array {
+		return array_map(function ($itemIssue) {
+			/** @var ItemIssue $itemIssue */
+			return $itemIssue->getIssue();
+		}, $this->issues->toArray());
+	}
+
+	public function getNextToReadIssues(): array {
+		return $this->next_to_read_issues;
+	}
+
+	public function setNextToReadIssues(): void {
+
+		$ROWS = 3;
+		$issues = $this->getIssuesArray();
+		$this->next_to_read_issues = [];
+
+		if($ROWS >= $this->getNumberIssues()) {
+			$this->next_to_read_issues = $issues;
+		}
+		else if($this->isDone()) {
+			// last three
+			$this->next_to_read_issues = array_slice($issues, -$ROWS, $ROWS, true);
+		}
+		else if(!$this->isStarted()) {
+			// first three
+			$this->next_to_read_issues = array_slice($issues, 0, $ROWS, false);
+		}
+		else {
+			/**
+			 * @var Issue $issue
+			 */
+			$indexFirst = 0;
+			$indexNext = 0;
+			$nextToReadIssue = $this->getNextToreadissue();
+			foreach($issues as $index => $issue) {
+				if($issue->getId() === $nextToReadIssue->getId()) {
+					$indexNext = $index;
+					break;
+				}
+
+			}
+
+//			$from = $indexNext - floor($ROWS / 2);
+			$to = $indexNext + ceil($ROWS / 2) - 1;
+			$from = $to - $ROWS;
+			if($from < 0) {
+				$from = 0;
+				$to = $ROWS - 1;
+			}
+
+//			if(!isset($issues[$to])) {
+//				// last three
+//				$this->next_to_read_issues = array_slice($issues, -$ROWS, $ROWS, true);
+//			}
+//			else {
+			foreach($issues as $index => $issue) {
+				if($from <= $index && $index <= $to) {
+					$this->next_to_read_issues[] = $issue;
+				}
+				else if($index > $to) {
+					break;
+				}
+			}
+//			}
+//			dd($issues, [
+//				'$nextToReadIssue' => $nextToReadIssue,
+//				'$nextToReadIssue->id' => $nextToReadIssue->getId(),
+//				'$indexNext' => $indexNext,
+//				'from' => $from,
+//				'to' => $to,
+//			], $this->next_to_read_issues);
+
+
+//			if($this->getRemainingIssues() === 1) {
+//				// three with next at the end
+//			}
+//			else {
+			// three with next in the middle
+//			$from = $indexFirst;
+//			}
+		}
+
+//		foreach($this->issues as $itemIssue) {
+//			if($this->isDone()) {
+//				// last three
+//			}
+//			else if($this->isStarted()) {
+//				if($this->getRemainingIssues() === 1) {
+//					// three with next at the end
+//				}
+//				else {
+//					// three with next in the middle
+//				}
+//			}
+//			else {
+//				// first three
+//			}
+//
+//		}
 	}
 }
